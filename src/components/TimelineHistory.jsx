57af36d6ab51af54f41f3e41fdde3db2d9bf0149
ivanets <evgeniyivanets@gmail.com>
2026-02-10 23:04:54 +0000

    tl

--- src/components/TimelineHistory.jsx
+++ src/components/TimelineHistory.jsx
@@ -694,19 +694,7 @@ const TimelineHistory = ({ onDayChange, selectedId, onSelectIntake, isSelectingT
                         }}
                       />
                       {/* Colored Frame/Border for Subtype */}
-                      {subtype && (
-                        // <div
-                        //   className="absolute inset-0 rounded-2xl pointer-events-none transition-all duration-300"
-                        //   style={{
-                        //     border: '2px solid',
-                        //     borderColor: subtypeColor,
-                        //     boxShadow: isSelected ? subtypeGlow : 'none',
-                        //     opacity: 0.6,
-                        //     margin: '-2px'
-                        //   }}
-                        // />
-                      )}
-                      
+                  
                       <div
                         className="px-3 py-2 rounded-2xl border relative"
                         style={{
